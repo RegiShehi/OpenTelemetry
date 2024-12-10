1a138da929a0515e3c81f5e3897236cb0ab6c3da
Regi Shehi <regi_shehi@hotmail.com>
2024-12-10 16:43:45 +0100

    Add OpenTelemetry to console application

---
+++ src/ConsoleTool/ApplicationDiagnostics.cs
@@ -0,0 +1,10 @@
+﻿using System.Diagnostics;
+
+namespace ConsoleTool;
+
+public static class ApplicationDiagnostics
+{
+    public const string ActivitySourceName = "Console.Tool.Diagnostics";
+
+    public static readonly ActivitySource ActivitySource = new(ActivitySourceName);
+}
